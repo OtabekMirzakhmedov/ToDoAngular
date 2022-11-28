9b5962f2c2699518858032e8f1d34783dd3e3a9c
OtabekMirzakhmedov <otabek.mirzakhmedov@gmail.com>
2022-11-28 19:33:16 +0500

    todo repository test

--- ToDoApp.Tests/DataTests/ToDoRepositoryTests.cs
+++ ToDoApp.Tests/DataTests/ToDoRepositoryTests.cs
@@ -99,6 +99,40 @@ namespace ToDoApp.Tests.DataTests
             }).Using(new ToDoEqualityComparer()), message: "Update method works incorrect");
         }
 
+        [Test]
+        public async Task ToDoRepository_GetAllWithDetailsAsync_ReturnsWithIncludedEntities()
+        {
+            using var context = new AppDbContext(UnitTestHelper.GetUnitTestDbOptions());
+
+            var toDoRepository = new ToDoRepository(context);
+
+            var todos = await toDoRepository.GetAllWithDetailsAsync();
+            var todo = todos.FirstOrDefault(x => x.Id == 1);
+
+            var expectedStepsCount = 1;
+
+            Assert.That(todos, Is.EqualTo(ExpectedToDos).Using(new ToDoEqualityComparer()), message: "GetAllWithDetailsAsync method works incorrect");
+            Assert.That(todo.Steps.Count(), Is.EqualTo(expectedStepsCount), message: "GetAllWithDetailsAsync method doesnt't return included entities");
+            Assert.That(todo.Steps, Is.Not.Null, message: "GetByIdWithDetailsAsync method doesnt't return included entities");
+        }
+
+        [TestCase("user2")]
+        public async Task ToDoRepository_GetAllWithDetailsByUserIdAsync_ReturnsWithIncludedEntities(string id)
+        {
+            using var context = new AppDbContext(UnitTestHelper.GetUnitTestDbOptions());
+
+            var toDoRepository = new ToDoRepository(context);
+
+            var todos = await toDoRepository.GetAllWithDetailsByUserId(id);
+            var todo = todos.FirstOrDefault(x => x.Id == 2);
+
+            var expectedStepsCount = 2;
+
+            Assert.That(todos, Is.EqualTo(ExpectedToDosForGetByUserIdCheck).Using(new ToDoEqualityComparer()), message: "GetAllWithDetailsAsync method works incorrect");
+            Assert.That(todo.Steps.Count(), Is.EqualTo(expectedStepsCount), message: "GetAllWithDetailsAsync method doesnt't return included entities");
+            Assert.That(todo.Steps, Is.Not.Null, message: "GetAllByUserIdWithDetailsAsync method doesnt't return included entities");
+        }
+
 
 
 
@@ -106,6 +140,13 @@ namespace ToDoApp.Tests.DataTests
             new[]
             {
                 new ToDo { Id = 1, AppUserId = "user1", CreatedAt = new DateTime(2022, 11, 28), Deadline = new DateTime(2022, 11, 29), IsCompleted = false, Progress = 0, Text = "Organize meeting"},
+                new ToDo { Id = 2, AppUserId = "user2", CreatedAt = new DateTime(2022, 11, 27), Deadline = new DateTime(2022, 11, 29), IsCompleted = true, Progress = 25, Text = "Shopping list" },
+                new ToDo { Id = 3, AppUserId = "user2", CreatedAt = new DateTime(2022, 11, 28), Deadline = new DateTime(2022, 1, 12), IsCompleted = false, Progress = 0, Text = "Visit doctor" }
+            };
+
+        private static IEnumerable<ToDo> ExpectedToDosForGetByUserIdCheck =>
+            new[]
+            {
                 new ToDo { Id = 2, AppUserId = "user2", CreatedAt = new DateTime(2022, 11, 27), Deadline = new DateTime(2022, 11, 29), IsCompleted = true, Progress = 25, Text = "Shopping list" },
                 new ToDo { Id = 3, AppUserId = "user2", CreatedAt = new DateTime(2022, 11, 28), Deadline = new DateTime(2022, 1, 12), IsCompleted = false, Progress = 0, Text = "Visit doctor" }
             };
